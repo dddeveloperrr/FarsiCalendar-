d12a28cd797c31388000869bfa98853b542c997b
farhad <dddeveloperrr@users.noreply.github.com>
2020-02-01 19:14:23 +0330

    Add files via upload

---
+++ hejriShamsi.java
@@ -0,0 +1,331 @@
+import java.util.HashMap;
+
+public class hejriShamsi {
+
+
+
+	HashMap<Integer, String> farvardin = new HashMap<Integer, String>();
+	HashMap<Integer, String> ordibehesht = new HashMap<Integer, String>();
+	HashMap<Integer, String> khordad = new HashMap<Integer, String>();
+	HashMap<Integer, String> tir = new HashMap<Integer, String>();
+	HashMap<Integer, String> mordad = new HashMap<Integer, String>();
+        HashMap<Integer, String> shahrivar = new HashMap<Integer, String>();
+	HashMap<Integer, String> mehr = new HashMap<Integer, String>();
+	HashMap<Integer, String> aban = new HashMap<Integer, String>();
+
+	 /*Adding elements to HashMap*/
+	public hejriShamsi() {
+
+                farvardin.put(1, "");
+		farvardin.put(2, "");
+		farvardin.put(3, "");
+		farvardin.put(4, "");
+                farvardin.put(5, "");
+		farvardin.put(6, "زادروز آشو زرتشت، اَبَراِنسان بزرگ تاریخ-روز امید، روز شادباش نویسی-");
+		farvardin.put(7, "");
+		farvardin.put(8, "");
+		farvardin.put(9, "");
+		farvardin.put(10, "جشن آبانگاه-");
+		farvardin.put(11, "");
+		farvardin.put(12, "روز جمهوری اسلامی-");
+		farvardin.put(13, "جشن سیزده به در-");
+		farvardin.put(14, "");
+		farvardin.put(15, "");
+		farvardin.put(16, "");
+		farvardin.put(17, "سروش روز، جشن سروشگان-");
+		farvardin.put(18, "");
+		farvardin.put(19, "فروردین روز، جشن فروردینگان-");
+		farvardin.put(20, "");
+		farvardin.put(21, "");
+		farvardin.put(22, "");
+		farvardin.put(23, "روز دندانپزشک-");
+		farvardin.put(24, "");
+		farvardin.put(25, "روز بزرگداشت عطار نیشابوری-");
+		farvardin.put(26, "");
+		farvardin.put(27, "");
+		farvardin.put(28, "");
+		farvardin.put(29, "روز ارتش جمهوری اسلامی ایران-");
+		farvardin.put(30, "زاد روز حکیم سید اسماعیل جرجانی، روز علوم آزمایشگاهی-");
+		farvardin.put(31, "");
+
+
+
+		ordibehesht.put(1, "روز بزرگداشت سعدی-");
+		ordibehesht.put(2, "");
+		ordibehesht.put(3, "روزبزرگداشت شیخ بهایی، روزملی کارآفرینی-");
+		ordibehesht.put(4, "");
+		ordibehesht.put(5, "");
+		ordibehesht.put(6, "");
+		ordibehesht.put(7, "");
+		ordibehesht.put(8, "");
+		ordibehesht.put(9, "روزشوراها-");
+		ordibehesht.put(10, "جشن چهلم نوروز-روز ملی خلیج فارس-");
+		ordibehesht.put(11, "");
+		ordibehesht.put(12, "شهادت استاد مرتضی مطهری، روزمعلم-");
+		ordibehesht.put(13, "");
+		ordibehesht.put(14, "");
+		ordibehesht.put(15, "جشن بهاربد-روز شیراز-");
+		ordibehesht.put(16, "");
+		ordibehesht.put(17, "روز اسناد ملی و میراث مکتوب-");
+		ordibehesht.put(18, "");
+		ordibehesht.put(19, "");
+		ordibehesht.put(20, "");
+		ordibehesht.put(21, "");
+		ordibehesht.put(22, "");
+		ordibehesht.put(23, "");
+		ordibehesht.put(24, "");
+		ordibehesht.put(25, "روز بزرگداشت فردوسی-");
+		ordibehesht.put(26, "");
+		ordibehesht.put(27, "روز ارتباطات و روابط عمومی-");
+		ordibehesht.put(28, "روز بزرگداشت حکیم عمر خیام-");
+		ordibehesht.put(29, "");
+		ordibehesht.put(30, "");
+		ordibehesht.put(31, "");
+
+
+
+		khordad.put(1, "روز بهره وری و بهینه سازی مصرف-روز بزرگداشت ملاصدرا-");
+		khordad.put(2, "");
+		khordad.put(3, "فتح خرمشهر در عملیات بیت المقدس و روز مقاومت، ایثار و پیروزی-");
+		khordad.put(4, "روز دزفول، روز مقاومت و پایداری-");
+		khordad.put(5, "");
+		khordad.put(6, "خرداد روز،جشن خردادگان-");
+		khordad.put(7, "");
+		khordad.put(8, "");
+		khordad.put(9, "");
+		khordad.put(10, "");
+		khordad.put(11, "");
+		khordad.put(12, "");
+		khordad.put(13, "");
+		khordad.put(14, "رحلت حضرت امام خمینی-");
+		khordad.put(15, "قیام 15 خرداد-");
+		khordad.put(16, "");
+		khordad.put(17, "");
+		khordad.put(18, "");
+		khordad.put(19, "");
+		khordad.put(20, "");
+		khordad.put(21, "");
+		khordad.put(22, "");
+		khordad.put(23, "");
+		khordad.put(24, "");
+		khordad.put(25, "روز ملی گل وگیاه-");
+		khordad.put(26, "");
+		khordad.put(27, "روز جهاد کشاورزی-");
+		khordad.put(28, "");
+		khordad.put(29, "");
+		khordad.put(30, "");
+		khordad.put(31, "");
+
+
+
+		tir.put(1, "روز اصناف-جشن آب پاشونک، جشن آغاز تابستان-");
+		tir.put(2, "");
+		tir.put(3, "");
+		tir.put(4, "");
+		tir.put(5, "");
+		tir.put(6, "");
+		tir.put(7, "انفجار دفتر حزب جمهوری اسلامی و شهادت دکتر بهشتی و 72 نفر از اعضای حزب-روز قوه قضاییه-");
+		tir.put(8, "روز مبارزه با سلاح های شیمیایی و میکروبی-");
+		tir.put(9, "");
+		tir.put(10, "روز صنعت و معدن-");
+		tir.put(11, "");
+		tir.put(12, "");
+		tir.put(13, "تیر روز،جشن تیرگان-");
+		tir.put(14, "روز قلم-");
+		tir.put(15, "جشن خام خواری-");
+		tir.put(16, "");
+		tir.put(17, "");
+		tir.put(18, "");
+		tir.put(19, "");
+		tir.put(20, "");
+		tir.put(21, "");
+		tir.put(22, "");
+		tir.put(23, "");
+		tir.put(24, "");
+		tir.put(25, "روز بهزیستی و تامین اجتماعی-");
+		tir.put(26, "");
+		tir.put(27, "اعلام پذیرش قطعنامه 598 شورای امنیت از سوی ایران-");
+		tir.put(28, "");
+		tir.put(29, "");
+		tir.put(30, "");
+		tir.put(31, "");
+
+
+
+
+
+		mordad.put(1, "");
+		mordad.put(2, "");
+		mordad.put(3, "");
+		mordad.put(4, "");
+		mordad.put(5, "");
+		mordad.put(6, "روز ترویج آموزش های فنی و حرفه ای-");
+		mordad.put(7, "مرداد روز، جشن مردادگان-");
+		mordad.put(8, "روز بزرگداشت شیخ شهاب الدین سهروردی-");
+		mordad.put(9, "");
+		mordad.put(10, "جشن چله تابستان-");
+		mordad.put(11, "");
+		mordad.put(12, "");
+		mordad.put(13, "");
+		mordad.put(14, "صدور فرمان مشروطیت-");
+		mordad.put(15, "");
+		mordad.put(16, "");
+		mordad.put(17, "روز خبرنگار-");
+		mordad.put(18, "");
+		mordad.put(19, "");
+		mordad.put(20, "");
+		mordad.put(21, "");
+		mordad.put(22, "");
+		mordad.put(23, "");
+		mordad.put(24, "");
+		mordad.put(25, "");
+		mordad.put(26, "سالروز ورود آزادگانِ سرافراز به وطن-");
+		mordad.put(27, "");
+		mordad.put(28, "سالروز وقایع 28 مرداد و برکناری دکتر محمد مصدق-سالروز فاجعه آتش زدن سینما رکس آبادان-");
+		mordad.put(29, "");
+		mordad.put(30, "");
+		mordad.put(31, "");
+
+
+
+
+		shahrivar.put(1, "روز بزرگداشت ابوعلی سینا و روز پزشک-");
+		shahrivar.put(2, "آغاز هفته دولت-");
+		shahrivar.put(3, "");
+		shahrivar.put(4, "زادروز داراب (کوروش)-شهریور روز، جشن شهریورگان-روز کارمند-");
+		shahrivar.put(5, "روز بزرگداشت محمدبن زکریای رازی و روز داروساز-");
+		shahrivar.put(6, "");
+		shahrivar.put(7, "");
+		shahrivar.put(8, "انفجار در دفتر نخست‌وزیری جمهوری اسلامی ایران، روز مبارزه با تروریسم-");
+		shahrivar.put(9, "");
+		shahrivar.put(10, "");
+		shahrivar.put(11, "روزصنعت چاپ-");
+		shahrivar.put(12, "");
+		shahrivar.put(13, "روز بزرگداشت ابوریحان بیرونی-روز تعاون-");
+		shahrivar.put(14, "");
+		shahrivar.put(15, "");
+		shahrivar.put(16, "");
+		shahrivar.put(17, "قیام 17 شهریور-");
+		shahrivar.put(18, "");
+		shahrivar.put(19, "درگذشت آیت الله سید محمود طالقانی-");
+		shahrivar.put(20, "");
+		shahrivar.put(21, "روز سینما-");
+		shahrivar.put(22, "");
+		shahrivar.put(23, "");
+		shahrivar.put(24, "");
+		shahrivar.put(25, "");
+		shahrivar.put(26, "");
+		shahrivar.put(27, "روز شعر و ادب پارسی و روز بزرگداشت استاد شهریار-");
+		shahrivar.put(28, "");
+		shahrivar.put(29, "");
+		shahrivar.put(30, "روز گفتگوی تمدنها-");
+		shahrivar.put(31, "آغاز هفته دفاع مقدس-");
+
+
+		mehr.put(1,"آغاز حمله مغول به ایران در پاییز 598 خورشیدی-");
+		mehr.put(2,"");
+		mehr.put(3,"");
+		mehr.put(4,"");
+		mehr.put(5,"");
+		mehr.put(6,"");
+		mehr.put(7,"سقوط هواپیمای حامل جمعی از فرماندهان جنگ (کلاهدوز، نامجو، فلاحی، فکوری، جهان آرا) در سال 1360-روز آتش نشانی و ایمنی-");
+		mehr.put(8,"روزبزرگداشت مولوی-");
+		mehr.put(9,"");
+		mehr.put(10,"");
+		mehr.put(11,"");
+		mehr.put(12,"");
+		mehr.put(13,"روز نیروی انتظامی-");
+		mehr.put(14,"روز دامپزشکی-");
+		mehr.put(15,"");
+		mehr.put(16,"روز ملی کودک-مهر روز، جشن مهرگان-");
+		mehr.put(17,"");
+		mehr.put(18,"");
+		mehr.put(19,"");
+		mehr.put(20,"روز بزرگداشت حافظ-");
+		mehr.put(21,"جشن پیروزی کاوه و فریدون-");
+		mehr.put(22,"");
+		mehr.put(23,"");
+		mehr.put(24,"");
+		mehr.put(25,"");
+		mehr.put(26,"روز تربیت بدنی و ورزش-");
+		mehr.put(27,"");
+		mehr.put(28,"");
+		mehr.put(29,"");
+		mehr.put(30,"");
+
+                aban.put(1,"روز آمار و برنامه ریزی-");
+                aban.put(2,"");
+                aban.put(3,"");
+                aban.put(4,"");
+                aban.put(5,"");
+                aban.put(6,"");
+                aban.put(7,"سالروز ورود کوروش بزرگ به بابل در سال 539 پیش از میلاد-");
+                aban.put(8,"روز نوجوان-");
+                aban.put(9,"");
+                aban.put(10,"آبان روز، جشن آبانگان-");
+                aban.put(11,"");
+                aban.put(12,"");
+                aban.put(13,"روز دانش آموز-");
+                aban.put(14,"روز فرهنگ عمومی-");
+                aban.put(15,"جشن میانه پاییز-");
+                aban.put(16,"");
+                aban.put(17,"");
+                aban.put(18,"روز ملی کیفیت-");
+                aban.put(19,"");
+                aban.put(20,"");
+                aban.put(21,"");
+                aban.put(22,"");
+                aban.put(23,"");
+                aban.put(24,"روز کتاب و کتابخوانی-");
+                aban.put(25,"");
+                aban.put(26,"");
+                aban.put(27,"");
+                aban.put(28,"");
+                aban.put(29,"");
+                aban.put(30,"");
+
+
+	
+
+	}
+
+
+	public String get (int rooz, String maah) {
+
+		String returnvalue = "";
+
+		if (maah.equals("farvardin")) {
+
+			returnvalue = farvardin.get(rooz);
+
+		} else if (maah.equals("ordibehesht")) {
+
+			returnvalue = ordibehesht.get(rooz);
+
+		} else if (maah.equals("khordad")) {
+
+			returnvalue = khordad.get(rooz);
+
+		} else if (maah.equals("tir")) {
+
+			returnvalue = tir.get(rooz);
+
+		} else if (maah.equals("mordad")) {
+
+			returnvalue = mordad.get(rooz);
+
+		} else if (maah.equals("shahrivar")) {
+
+			returnvalue = shahrivar.get(rooz);
+
+		} else if (maah.equals("mehr")) {
+
+			returnvalue = mehr.get(rooz);
+		} else if (maah.equals("aban")) {
+
+			returnvalue = aban.get(rooz);
+		}
+
+		return returnvalue;
+	}
+}
